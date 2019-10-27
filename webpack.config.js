1e23105923cf260756b1af5bbb706371f60a5da0
Roman Kalhin <Kalginroman@gmail.com>
2019-10-27 21:21:53 +0200

    Install Angular

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    app: "./src/index.js"
+    bundle: "./src/index.js"
   },
   output: {
     filename: "[name].js",

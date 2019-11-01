c7731d634083d91a9ba3fa638e2d011634a3dfba
kalhin <kalginroman@gmail.com>
2019-11-01 15:11:34 +0200

    Add SIGN UP form with validation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,55 @@
-// import "./css/main.css"
 const angular = require("angular");
+
 const appModule = angular.module("myApp", []);
 
 appModule.controller("appCtrl", function($scope) {
-    $scope.name = "Hello"
+  const vm = this;
+
+  const userNameRegexp = new RegExp(/^[a-zA-Z0-9_-]{5,15}$/); //low and up case and from 5 to 15 characters or numbers
+  const userPasswRegexp = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/); // Minimum 6 characters, at least one letter and one number
+
+  $scope.user = {};
+  let user = $scope.user;
+
+  user.name = null;
+  user.isUserNameValidate = false;
+  user.nameErrorMessage = null;
+  let nameErrorMessage = "must be from 5 to 15 digits or letters of the Latin alphabet";
+
+  user.password = null;
+  user.isUserPasswValidate = false;
+  user.passwErrorMessage = null;
+  let passwErrorMessage = "must be minimum 6 characters, at least one letter and one number"
+
+  user.repeatPassword = null;
+  user.isRepeatPasswValidate = false;
+  user.repeatPasswErrorMessage = null;
+  const repeatPasswErrorMessage = "password does not match"
+
+  user.isRememberPassword = true;
+
+  $scope.$watch("user.name", function(newValue) {
+    user.isUserNameValidate = userNameRegexp.test(newValue);
+    console.log(user.isUserNameValidate);
+  });
+
+  $scope.$watch("user.password", function(newValue) {
+    user.isUserPasswValidate = userPasswRegexp.test(newValue);
+    console.log(user.isUserPasswValidate);
+  });
+
+  $scope.$watch("user.repeatPassword", function(newValue) {
+    user.password === newValue && user.password !== null
+      ? (user.isRepeatPasswValidate = true)
+      : (user.isRepeatPasswValidate = false);
+    console.log(user.isRepeatPasswValidate);
+  });
+
+  $scope.signUp = function() {
+    user.isUserNameValidate &&
+    user.isUserPasswValidate &&
+    user.isRepeatPasswValidate
+      ? console.log(true)
+      : console.log(false);
+  };
 });
